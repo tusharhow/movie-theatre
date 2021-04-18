0f530ccad205a81d8a1377609875c0b56c27944b
tusharhow <tusharsoftdev@gmail.com>
2021-04-18 20:46:31 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/movie_theatre/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.movie_theatre
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/data.dart
@@ -0,0 +1,13 @@
+List<String> images = [
+  "images/blade.jpg",
+  "images/mad.jpg",
+  "images/new.jpg",
+  "images/komola.jpg",
+];
+
+List<String> title = [
+  "Blade Runner",
+  "Mad Max",
+  "The Heat",
+  "Komola Rocket",
+];
